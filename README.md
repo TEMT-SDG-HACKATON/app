d0508d659beb41858d2aaaa63992638124fa952c
Toni <dannyore001@gmail.com>
2023-11-30 13:11:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sdg_hackaton_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sdg_hackaton_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sdg_hackaton_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
